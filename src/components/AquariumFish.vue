e663d9939a22f339c1eebff433ccd8c55a9b4f1f
Adam Cummings <adam_cummings_@outlook.com>
2024-02-06 18:43:05 -0600

    addressed all ts issues

--- src/components/AquariumFish.vue
+++ src/components/AquariumFish.vue
@@ -28,19 +28,25 @@ export default {
         }
     },
     mounted() {
+        // @ts-ignore
         this.tlm = gsap.timeline({ repeat: -1, paused: true })
 
         this.$nextTick(() => {
+            // @ts-ignore
             this.animate(this.tlm);
+            // @ts-ignore
             this.tlm.play(this.getRandomStartTime());
         });
     },
     watch: {
         sizeChange() {
+            // @ts-ignore
             this.$refs.fish.setAttribute('width', this.fishWidth)
+            // @ts-ignore
             this.$refs.fish.setAttribute('x', this.tankXPosition)
-
+            // @ts-ignore
             this.tlm.invalidate()
+            // @ts-ignore
             this.tlm.play(this.getRandomStartTime())
         },
     },
@@ -69,8 +75,8 @@ export default {
         },
         animate(timeline: typeof gsap.timeline) {
             const randomYPos = this.getRandomStartY()
-            timeline
-            .set(this.$refs.fishContainer as SVGElement, { y: randomYPos, x: () => { return this.fishWidth + 20 }, scaleX: -1 })
+            // @ts-ignore
+            timeline.set(this.$refs.fishContainer as SVGElement, { y: randomYPos, x: () => { return this.fishWidth + 20 }, scaleX: -1 })
               .to(this.$refs.fishContainer, { y: this.posYEnd, x: this.xMovement, duration: this.getSize(this.size) / 10, ease: this.fishAnimation.ease })
               .to(this.$refs.fishContainer, { scaleX: 1, duration: 0, x: () => { return `-=${this.fishWidth}` } })
               .to(this.$refs.fishContainer, { y: randomYPos, x: 20, duration: this.getSize(this.size) / 10, ease: this.fishAnimation.ease });
